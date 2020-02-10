587b2b4631cb13c89122ddb809b5243948c428f3
g1.kwon <spring_shot@naver.com>
2020-02-11 03:17:06 +0900

    add 참조변수

---
+++ src/reference/StudentTest.java
@@ -0,0 +1,20 @@
+package reference;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+
+		Student studentLee = new Student(100, "Lee");
+		studentLee.setKoreaSubject("국어", 100);
+		studentLee.setMathSubject("수학", 100);
+
+		Student studentKim = new Student(100, "Kim");
+		studentKim.setKoreaSubject("국어", 100);
+		studentKim.setMathSubject("수학", 100);
+
+		studentLee.showStudentScore();
+		studentKim.showStudentScore();
+
+	}
+
+}
